06a50f05c2ee2bc7c2cd4115bc6d2ce3e8701188
abhil <abhil@LAPTOP-13NC4DCI>
2020-12-09 19:19:19 +0530

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using BakedInHeaven.BusinessServices;
+using BakedInHeaven.BusinessServices.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakedInHeaven.API.Controllers
+{
+    [Route("products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsService _productsService;
+        public ProductsController(IProductsService productsService)
+        {
+            _productsService = productsService;
+        }
+        [HttpGet]
+        public IEnumerable<ProductDto> GetAllProducts()
+        {
+
+            return _productsService.GetAllProducts();
+        }
+        [HttpPost]
+        public void AddProduct(ProductDto product)
+        {
+            _productsService.AddProduct(product);
+        }
+        [HttpDelete]
+        public void DeleteProduct(ProductDto product)
+        {
+            _productsService.DeleteProduct(product);
+        }
+        [HttpPut]
+        public void UpdateProduct(ProductDto product)
+        {
+            _productsService.UpdateProduct(product);
+        }
+    }
+}
